1b5feb210a11d0a1b8ec80b3254b0cb990feeefe
Aakash Patel <aakashpatel25274@gmail.com>
2024-05-18 22:11:46 -0400

    Math power function

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-import "./style.css";
-
 // import BinarySearch from "./search/binary";
 
 // import findPosition from "./search/binary/findPosition";
+var myPow = function (x, n) {
+  if (n === 0) {
+    return 1;
+  }
+
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+
+  if (n % 2 === 0) {
+    let halfPower = myPow(x, n / 2);
+    return halfPower * halfPower;
+  } else {
+    return x * myPow(x, n - 1);
+  }
+};
+
+console.log(myPow(2.0, 10));
